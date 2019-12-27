d706975fb558d4da0287fdaa095453423486af80
michal-z <michalziulek@gmail.com>
2019-12-27 23:48:32 +0100

    -

---
+++ src/d3d12.rs
@@ -0,0 +1,259 @@
+use std::mem;
+use std::ops::Deref;
+use std::option::Option;
+use std::ptr;
+use winapi::um::d3d12::*;
+use winapi::um::unknwnbase::IUnknown;
+use winapi::um::winnt::HRESULT;
+use winapi::Interface;
+
+#[repr(transparent)]
+pub struct WeakPtr<T>(*mut T);
+
+impl<T> WeakPtr<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self(ptr::null_mut())
+    }
+
+    pub fn from_raw(ptr: *mut T) -> Self {
+        let r = unsafe { ptr.as_mut().unwrap() };
+        Self(r as *mut T)
+    }
+
+    pub fn as_raw(&self) -> *mut T {
+        self.0 as *mut T
+    }
+
+    pub fn is_null(&self) -> bool {
+        self.0 == ptr::null_mut()
+    }
+}
+
+impl<T> Deref for WeakPtr<T> {
+    type Target = T;
+    #[inline]
+    fn deref(&self) -> &T {
+        unsafe { &*self.0 }
+    }
+}
+
+impl<T> Clone for WeakPtr<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for WeakPtr<T> {}
+
+impl<T: Interface> WeakPtr<T> {
+    pub fn release(&mut self) -> u32 {
+        let mut refcount: u32 = 0;
+        if self.0 != ptr::null_mut() {
+            unsafe {
+                refcount = (&*(self.0 as *mut _ as *mut IUnknown)).Release();
+            }
+            self.0 = ptr::null_mut();
+        }
+        refcount
+    }
+}
+
+pub type Dx12Device = WeakPtr<ID3D12Device2>;
+pub type Dx12CommandQueue = WeakPtr<ID3D12CommandQueue>;
+pub type Dx12GraphicsCommandList = WeakPtr<ID3D12GraphicsCommandList1>;
+pub type Dx12Resource = WeakPtr<ID3D12Resource>;
+
+impl Dx12Device {
+    #[inline]
+    pub fn create_shader_resource_view(
+        &self,
+        resource: Option<Dx12Resource>,
+        desc: Option<&D3D12_SHADER_RESOURCE_VIEW_DESC>,
+        dest_descriptor: D3D12_CPU_DESCRIPTOR_HANDLE,
+    ) {
+        let resource = match resource {
+            Some(r) => r.as_raw(),
+            None => ptr::null_mut(),
+        };
+        let desc = match desc {
+            Some(d) => d as *const _,
+            None => ptr::null(),
+        };
+        unsafe { self.CreateShaderResourceView(resource, desc, dest_descriptor) };
+    }
+}
+
+impl Dx12GraphicsCommandList {
+    #[inline]
+    pub fn rs_set_viewports(&self, viewports: &[D3D12_VIEWPORT]) {
+        unsafe { self.RSSetViewports(viewports.len() as u32, viewports.as_ptr() as *const _) };
+    }
+
+    #[inline]
+    pub fn rs_set_scissor_rects(&self, rects: &[D3D12_RECT]) {
+        unsafe { self.RSSetScissorRects(rects.len() as u32, rects.as_ptr() as *const _) };
+    }
+
+    #[inline]
+    pub fn om_set_render_target(
+        &self,
+        render_target_descriptor: D3D12_CPU_DESCRIPTOR_HANDLE,
+        depth_stencil_descriptor: Option<D3D12_CPU_DESCRIPTOR_HANDLE>,
+    ) {
+        let ds = if depth_stencil_descriptor.is_none() {
+            ptr::null()
+        } else {
+            &depth_stencil_descriptor.unwrap()
+        };
+        unsafe { self.OMSetRenderTargets(1, &render_target_descriptor, 1, ds) };
+    }
+
+    #[inline]
+    pub fn clear_render_target_view(
+        &self,
+        render_target_view: D3D12_CPU_DESCRIPTOR_HANDLE,
+        color_rgba: &[f32; 4],
+        rects: &[D3D12_RECT],
+    ) {
+        let (num_rects, rects) = if rects.is_empty() {
+            (0 as u32, ptr::null())
+        } else {
+            (rects.len() as u32, rects.as_ptr() as *const _)
+        };
+        unsafe {
+            self.ClearRenderTargetView(
+                render_target_view,
+                color_rgba.as_ptr() as *const _,
+                num_rects,
+                rects,
+            )
+        };
+    }
+
+    #[inline]
+    pub fn copy_buffer_region(
+        &self,
+        dst_buffer: Dx12Resource,
+        dst_offset: u64,
+        src_buffer: Dx12Resource,
+        src_offset: u64,
+        num_bytes: u64,
+    ) {
+        unsafe {
+            self.CopyBufferRegion(
+                dst_buffer.as_raw(),
+                dst_offset,
+                src_buffer.as_raw(),
+                src_offset,
+                num_bytes,
+            )
+        };
+    }
+
+    #[inline]
+    pub fn ia_set_vertex_buffers(&self, start_slot: u32, views: &[D3D12_VERTEX_BUFFER_VIEW]) {
+        assert!(!views.is_empty());
+        unsafe {
+            self.IASetVertexBuffers(start_slot, views.len() as u32, views.as_ptr() as *const _)
+        };
+    }
+
+    #[inline]
+    pub fn ia_set_primitive_topology(&self, primitive_topology: D3D12_PRIMITIVE_TOPOLOGY) {
+        unsafe { self.IASetPrimitiveTopology(primitive_topology) };
+    }
+
+    #[inline]
+    pub fn draw_instanced(
+        &self,
+        vertex_count_per_instance: u32,
+        instance_count: u32,
+        start_vertex_location: u32,
+        start_instance_location: u32,
+    ) {
+        unsafe {
+            self.DrawInstanced(
+                vertex_count_per_instance,
+                instance_count,
+                start_vertex_location,
+                start_instance_location,
+            )
+        };
+    }
+
+    #[inline]
+    pub fn set_graphics_root_shader_resource_view(
+        &self,
+        root_parameter_index: u32,
+        buffer_location: D3D12_GPU_VIRTUAL_ADDRESS,
+    ) {
+        unsafe { self.SetGraphicsRootShaderResourceView(root_parameter_index, buffer_location) };
+    }
+
+    #[inline]
+    pub fn set_graphics_root_descriptor_table(
+        &self,
+        root_parameter_index: u32,
+        base_descriptor: D3D12_GPU_DESCRIPTOR_HANDLE,
+    ) {
+        unsafe { self.SetGraphicsRootDescriptorTable(root_parameter_index, base_descriptor) };
+    }
+
+    #[inline]
+    pub fn set_graphics_root_32bit_constant(
+        &self,
+        root_parameter_index: u32,
+        src_data: u32,
+        dest_offset_in_32bit_values: u32,
+    ) {
+        unsafe {
+            self.SetGraphicsRoot32BitConstant(
+                root_parameter_index,
+                src_data,
+                dest_offset_in_32bit_values,
+            )
+        };
+    }
+
+    #[inline]
+    pub fn set_graphics_root_32bit_constants<T>(
+        &self,
+        root_parameter_index: u32,
+        src_data: &[T],
+        dest_offset_in_32bit_values: u32,
+    ) {
+        assert_eq!(mem::size_of::<T>(), 4);
+        assert!(!src_data.is_empty());
+        unsafe {
+            self.SetGraphicsRoot32BitConstants(
+                root_parameter_index,
+                src_data.len() as u32,
+                src_data.as_ptr() as *const _,
+                dest_offset_in_32bit_values,
+            )
+        };
+    }
+
+    #[inline]
+    pub fn close(&self) -> HRESULT {
+        let hr = unsafe { self.Close() };
+        assert_eq!(hr, 0);
+        hr
+    }
+}
+
+impl Dx12Resource {
+    #[inline]
+    pub fn get_gpu_virtual_address(&self) -> D3D12_GPU_VIRTUAL_ADDRESS {
+        unsafe { self.GetGPUVirtualAddress() }
+    }
+}
+
+impl Dx12CommandQueue {
+    #[inline]
+    pub fn execute_command_lists(&self, command_lists: &[*mut ID3D12CommandList]) {
+        assert!(!command_lists.is_empty());
+        unsafe { self.ExecuteCommandLists(command_lists.len() as u32, command_lists.as_ptr()) };
+    }
+}
---
+++ src/misc.rs
@@ -0,0 +1,176 @@
+use std::ffi::CString;
+use std::mem;
+use std::ptr;
+use winapi::shared::minwindef::{LPARAM, LRESULT, UINT, WPARAM};
+use winapi::shared::windef::{HWND, RECT};
+use winapi::um::libloaderapi::GetModuleHandleA;
+use winapi::um::profileapi::{QueryPerformanceCounter, QueryPerformanceFrequency};
+use winapi::um::unknwnbase::IUnknown;
+use winapi::um::winnt::{LARGE_INTEGER, LPCSTR};
+use winapi::um::winuser::{
+    AdjustWindowRect, CreateWindowExA, DefWindowProcA, DispatchMessageA, LoadCursorA, PeekMessageA,
+    PostQuitMessage, RegisterClassA, SetProcessDPIAware, SetWindowTextA, CW_USEDEFAULT, IDC_ARROW,
+    MSG, PM_REMOVE, VK_ESCAPE, WM_DESTROY, WM_KEYDOWN, WM_QUIT, WNDCLASSA, WS_CAPTION,
+    WS_MINIMIZEBOX, WS_OVERLAPPED, WS_SYSMENU, WS_VISIBLE,
+};
+use winapi::Interface;
+
+#[macro_export]
+macro_rules! vhr {
+    ($name:expr) => {
+        unsafe {
+            let hr = $name;
+            assert_eq!(hr, 0);
+        };
+    };
+}
+
+pub struct FrameStats {
+    pub time: f64,
+    pub delta_time: f32,
+    previous_time: f64,
+    header_refresh_time: f64,
+    num_frames: u64,
+    start_counter: LARGE_INTEGER,
+    frequency: LARGE_INTEGER,
+}
+
+unsafe extern "system" fn wndproc(
+    window: HWND,
+    message: UINT,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    let handled = match message {
+        WM_DESTROY => {
+            PostQuitMessage(0);
+            true
+        }
+        WM_KEYDOWN => {
+            if wparam == VK_ESCAPE as usize {
+                PostQuitMessage(0);
+                true
+            } else {
+                false
+            }
+        }
+        _ => false,
+    };
+
+    if handled {
+        0
+    } else {
+        DefWindowProcA(window, message, wparam, lparam)
+    }
+}
+
+pub fn create_window(name: &CString, width: u32, height: u32) -> HWND {
+    unsafe {
+        SetProcessDPIAware();
+
+        let mut winclass: WNDCLASSA = mem::zeroed();
+        winclass.lpfnWndProc = Some(wndproc);
+        winclass.hInstance = GetModuleHandleA(ptr::null());
+        winclass.hCursor = LoadCursorA(ptr::null_mut(), IDC_ARROW as LPCSTR);
+        winclass.lpszClassName = name.as_ptr();
+        RegisterClassA(&winclass);
+    }
+
+    let style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX;
+    let mut rect = RECT {
+        left: 0,
+        top: 0,
+        right: width as i32,
+        bottom: height as i32,
+    };
+    unsafe {
+        AdjustWindowRect(&mut rect, style, 0);
+        CreateWindowExA(
+            0,
+            name.as_ptr(),
+            name.as_ptr(),
+            style | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            rect.right - rect.left,
+            rect.bottom - rect.top,
+            ptr::null_mut(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+        )
+    }
+}
+
+pub fn handle_window_messages() -> bool {
+    unsafe {
+        let mut message: MSG = mem::zeroed();
+        while PeekMessageA(&mut message, ptr::null_mut(), 0, 0, PM_REMOVE) != 0 {
+            DispatchMessageA(&message);
+            if message.message == WM_QUIT {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+impl FrameStats {
+    pub fn new() -> Self {
+        let mut start_counter: LARGE_INTEGER = unsafe { mem::zeroed() };
+        let mut frequency: LARGE_INTEGER = unsafe { mem::zeroed() };
+        unsafe {
+            QueryPerformanceCounter(&mut start_counter as *mut _ as *mut LARGE_INTEGER);
+            QueryPerformanceFrequency(&mut frequency as *mut _ as *mut LARGE_INTEGER);
+        }
+        Self {
+            time: 0.0,
+            delta_time: 0.0,
+            previous_time: -1.0,
+            header_refresh_time: 0.0,
+            num_frames: 0,
+            start_counter,
+            frequency,
+        }
+    }
+
+    pub fn update(&mut self, window: HWND, name: &CString) {
+        if self.previous_time < 0.0 {
+            self.previous_time = self.time();
+            self.header_refresh_time = self.previous_time;
+        }
+
+        self.time = self.time();
+        self.delta_time = (self.time - self.previous_time) as f32;
+        self.previous_time = self.time;
+
+        if (self.time - self.header_refresh_time) >= 1.0 {
+            let fps = (self.num_frames as f64) / (self.time - self.header_refresh_time);
+            let ms = (1.0 / fps) * 1000.0;
+            let header = CString::new(format!(
+                "[{:.1} fps  {:.3} ms] {}",
+                fps,
+                ms,
+                name.to_str().unwrap()
+            ))
+            .unwrap();
+            unsafe {
+                SetWindowTextA(window, header.as_ptr());
+            }
+            self.header_refresh_time = self.time;
+            self.num_frames = 0;
+        }
+        self.num_frames += 1;
+    }
+
+    pub fn time(&self) -> f64 {
+        let mut counter: LARGE_INTEGER = unsafe { mem::zeroed() };
+        unsafe { QueryPerformanceCounter(&mut counter as *mut _ as *mut LARGE_INTEGER) };
+
+        let counter = unsafe { *counter.QuadPart() };
+        let start_counter = unsafe { *self.start_counter.QuadPart() };
+        let frequency = unsafe { *self.frequency.QuadPart() };
+
+        ((counter - start_counter) as f64) / (frequency as f64)
+    }
+}
